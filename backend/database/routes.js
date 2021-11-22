b51caa1ea763d9aa777725b45bf44c3f68db8c3e
Sam Timmins <stimmins@smu.edu>
2021-11-21 21:44:01 -0600

    Login route

--- backend/database/routes.js
+++ backend/database/routes.js
@@ -96,5 +96,53 @@ module.exports = function routes(app, logger) {
         });
     });
 
+    app.post("/login", (req, res) => {
+        // if there is no issue obtaining a connection, execute query and release connection
+        const username = req.body.username;
+        var sql = "SELECT * FROM fitfindr.users WHERE username = ?";
+        pool.query(sql, [username], (err, rows) => {
+            if (err || !rows.length) {
+                logger.error("Error while username salt: \n", err);
+                res.status(400).send({ 
+                    success: false, 
+                    msg: "Invalid username or password" 
+                });
+            } else {
+                const hash = rows[0]["password"];
+                const password = req.body.password;
+        
+                bcrypt.compare(password, hash, (err, result) => {
+                    if (result && !err) {
+                        let id = rows[0]["id"];
+                        let username = rows[0]["username"];
+                        let chest = rows[0]["chest"];
+                        let height = rows[0]["height"];
+                        let hip = rows[0]["hip"];
+                        let sleeveLength = rows[0]["sleeveLength"];
+                        let neck = rows[0]["neck"];
+                        let gender = rows[0]["gender"];
 
+                        res.status(200).send({
+                            success: true,
+                            data: { id: id, 
+                                    username: username, 
+                                    chest: chest, 
+                                    height: height, 
+                                    hip: hip, 
+                                    sleeveLength: sleeveLength, 
+                                    neck: neck, 
+                                    gender: gender 
+                            }
+                        });
+                    } else {
+                        logger.error("Error no matching password: \n", err);
+                        res.status(400).send({
+                            success: false,
+                            msg: "Incorrect username or password"
+                        });
+                    }
+                });
+            }
+        });
+    });
 }
